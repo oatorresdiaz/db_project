3ac058e1361ebb146a243a3bfea22739b180449d
RobertoPerezMorales <roberto.perez17@upr.edu>
2017-12-03 21:02:16 -0400

    Merge branch 'Handlers_OrlandoTorres' of https://github.com/orlandoecool/db_project into Handlers_RobertoPerez
    # Conflicts:
    #	src/handler/PurchasesHandler.java

--- src/dao/PurchasesDao.java
+++ src/dao/PurchasesDao.java
@@ -9,28 +9,28 @@ public class PurchasesDao {
         ArrayList<Object[]> testPurchases = new ArrayList<>();
 
         Object[] purchase1 = new Object[4];
-        purchase1[0] = 0;
-        purchase1[1] = 1;
-        purchase1[2] = 0;
-        purchase1[3] = 20;
+        purchase1[0] = 1;
+        purchase1[1] = 0;
+        purchase1[2] = 20;
+        purchase1[3] = "12/2/2016";
 
         Object[] purchase2 = new Object[4];
-        purchase2[0] = 1;
+        purchase2[0] = 0;
         purchase2[1] = 0;
-        purchase2[2] = 0;
-        purchase2[3] = 10;
+        purchase2[2] = 10;
+        purchase2[3] = "07/25/2017";
 
         Object[] purchase3 = new Object[4];
-        purchase3[0] = 2;
-        purchase3[1] = 1;
-        purchase3[2] = 2;
-        purchase3[3] = 100;
+        purchase3[0] = 1;
+        purchase3[1] = 2;
+        purchase3[2] = 100;
+        purchase3[3] = "10/2/2017";
 
         Object[] purchase4 = new Object[4];
-        purchase4[0] = 3;
-        purchase4[1] = 0;
-        purchase4[2] = 1;
-        purchase4[3] = 25;
+        purchase4[0] = 0;
+        purchase4[1] = 1;
+        purchase4[2] = 25;
+        purchase4[3] = "02/14/2016";
 
         testPurchases.add(purchase1);
         testPurchases.add(purchase2);
--- src/handler/PurchasesHandler.java
+++ src/handler/PurchasesHandler.java
@@ -60,44 +60,4 @@ public class PurchasesHandler {
         return results;
     }
 
-
-
-    //FOR TESTING PURPOSES
-    private static ArrayList<Object[]> getTestPurchases(){
-        ArrayList<Object[]> testPurchases = new ArrayList<>();
-
-        Object[] purchase1 = new Object[4];
-        purchase1[0] = 1;
-        purchase1[1] = 0;
-        purchase1[2] = 20;
-        purchase1[3] = "12/2/2016";
-
-        Object[] purchase2 = new Object[4];
-        purchase2[0] = 0;
-        purchase2[1] = 0;
-        purchase2[2] = 10;
-        purchase2[3] = "07/25/2017";
-
-        Object[] purchase3 = new Object[4];
-        purchase3[0] = 1;
-        purchase3[1] = 2;
-        purchase3[2] = 100;
-        purchase3[3] = "10/2/2017";
-
-        Object[] purchase4 = new Object[4];
-        purchase4[0] = 0;
-        purchase4[1] = 1;
-        purchase4[2] = 25;
-        purchase4[3] = "02/14/2016";
-
-        testPurchases.add(purchase1);
-        testPurchases.add(purchase2);
-        testPurchases.add(purchase3);
-        testPurchases.add(purchase4);
-
-        return testPurchases;
-    }
-
-
-
 }
