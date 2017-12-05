359adc5f9ebf5748a8f4ec7515f18c3188326928
RobertoPerezMorales <roberto.perez17@upr.edu>
2017-12-04 21:29:22 -0400

    Added Utility JoinLinkedHashMap

---
+++ src/utilities/JoinLinkedHashMaps.java
@@ -0,0 +1,33 @@
+package utilities;
+
+
+import java.util.LinkedHashMap;
+
+public class JoinLinkedHashMaps {
+
+    public static LinkedHashMap<String, Object> joinWithEqualArg(LinkedHashMap<String,
+            Object> map1, LinkedHashMap<String, Object> map2, String sameKey){
+
+        LinkedHashMap<String, Object> joinedList = new LinkedHashMap<>();
+
+        if(map1.containsKey(sameKey) && map2.containsKey(sameKey)){
+            Object[] mapKeys1 = map1.keySet().toArray();
+            Object[] mapKeys2 = map2.keySet().toArray();
+
+            for(int i = 0; i < map1.size(); i++){
+                joinedList.put((String) mapKeys1[i], map1.get(mapKeys1));
+            }
+            for(int i = 0; i < map2.size(); i++){
+                if(mapKeys2[i] != sameKey) {
+                    joinedList.put((String) mapKeys2[i], map2.get(mapKeys2));
+                }
+            }
+        }
+        return joinedList;
+
+    }
+
+
+
+
+}
