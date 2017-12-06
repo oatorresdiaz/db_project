c531c94a2094efc506b7d6a2e92946cb306adae2
orlandotorres <orlando.torres12@upr.edu>
2017-12-05 20:43:43 -0400

    Orlando Torres:

--- src/utilities/HardWiredUtility.java
+++ src/utilities/HardWiredUtility.java
@@ -1,15 +1,36 @@
 package utilities;
 
+import handler.*;
+
 import java.util.ArrayList;
 import java.util.LinkedHashMap;
 
 /*****THIS CLASS IS FOR PROJECT PHASE 1 PURPOSES ONLY.*****/
 public class HardWiredUtility {
 
+    //User
+    private static UsersHandler usrs = new UsersHandler();
+    //User subcategories
+    private static AdminsHandler admns = new AdminsHandler();
+    private static SuppliersHandler spplrs = new SuppliersHandler();
+    private static RequestersHandler rqstr = new RequestersHandler();
+    //Resources
+    private static ResourcesHandler rs = new ResourcesHandler();
+    //Resources relations
+    private static InventoryHandler inv = new InventoryHandler();
+    private static RequestsHandler rqsts = new RequestsHandler();
+    //Requester and Inventory relations
+    private static ReserveHandler rsrv = new ReserveHandler();
+    private static PurchasesHandler prchs = new PurchasesHandler();
+
     private static JoinLinkedHashMaps JLHM = new JoinLinkedHashMaps();
 
     public static ArrayList<LinkedHashMap<String, Object>> listNIJ(ArrayList<LinkedHashMap<String, Object>> list1, ArrayList<LinkedHashMap<String, Object>> list2) {
         String key = sameKey(list1, list2);
+        if(key == null){
+            list1.clear();
+            return list1;
+        }
         ArrayList<LinkedHashMap<String, Object>> result = new ArrayList<>();
         LinkedHashMap<String, Object> element;
         for (int i = 0; i < list1.size(); i++) {
